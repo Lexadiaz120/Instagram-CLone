2c2cfed0088c6710eaa280a427fe38670d9e8a3d
unknown <daura38@gmail.com>
2022-07-06 15:37:47 +0200

    add contexts

---
+++ src/contexts/UserTokenContext.js
@@ -0,0 +1,19 @@
+import { createContext, useContext, useState } from "react";
+import useLocalStorage from "../hooks/useLocalStorage";
+export const UserTokenContext = createContext();
+
+export const UserTokenContextProvider = ({ children }) => {
+  const [token, setToken] = useLocalStorage("token", "");
+  const [userId, UseUserId] = useLocalStorage("id", "");
+  console.log(userId);
+
+  return (
+    <UserTokenContext.Provider value={{ token, setToken, userId, UseUserId }}>
+      {children}
+    </UserTokenContext.Provider>
+  );
+};
+
+export const useUserTokenContext = () => {
+  return useContext(UserTokenContext);
+};
