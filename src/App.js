be2cff715648b5303be73be5d7f7ef2ea320908d
unknown <daura38@gmail.com>
2022-07-07 21:36:21 +0200

    make changes in App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
 import RegisterForm from "./components/RegisterForm/RegisterForm";
+import Header from "./components/Header/Header";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { LoginPage } from "./pages/LoginPage/LoginPage";
 import { ProfilePage } from "./pages/ProfilePage/ProfilePage";
 import { UserSettings } from "./components/UserSettings/UserSettings";
+import CreatePostForm from "./components/CreatePostForm/CreatePostForm";
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <BrowserRouter>
         <Routes>
           <Route path={"/"} element={<LoginPage />}></Route>
