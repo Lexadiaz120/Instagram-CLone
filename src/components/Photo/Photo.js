254cdbf53dc9110aeeef62226c9da6a2b7d7f76d
unknown <daura38@gmail.com>
2022-07-08 17:24:35 +0200

    make modifications in Photo

--- src/components/Photo/Photo.js
+++ src/components/Photo/Photo.js
@@ -1,40 +1,57 @@
 import Avatar from "../Avatar/Avatar";
 import useUser from "../../hooks/useUser";
 import Button from "../Button/Button";
+import { useState } from "react";
+import { DetailsModal } from "../DetailsModal/DetailsModal";
+import { CommentButton } from "../CommentButton/CommentButton";
+import { LikeButton } from "../LikeButton/LikeButton";
 
 const Photo = ({ photo }) => {
-
-    const { user, loading, error } = useUser();
-    console.log("user?avatar" + user?.avatar);    
-    
-
-    const{name_photo, username, description_photo} = photo;
-
-    return(
-        <article>
-            <header className="header"> 
-                <div className="perfilImage">
-                    <Avatar avatar={user?.avatar} username={user?.username} />
-                </div>
-                <div className="photoDatas">                    
-                    <p>{username}</p>
-                </div>
-                                            
-            </header>
-            <div className="bodyPhoto">
-                <img src={`${process.env.REACT_APP_API_URL}/${name_photo}`} alt="Photo"/>
-            </div>
-            <div className="post-content">
-                <div className="iconsPhoto">
-                    <Button>{"🤍"}</Button>
-                    <Button>{"💬"}</Button>                   
-                </div>
-                <p className="likes">1000 likes</p>
-                <p className="description"><span>{username}</span>{description_photo}</p> 
-            </div> 
-                                                                       
-        </article>
-    );    
+  console.log(photo, "soy photo");
+  const { user, loading, error } = useUser();
+  const [modal, setModal] = useState(false);
+  const [commentInput, setCommentInput] = useState(false);
+  console.log(commentInput);
+  const { name_photo, id, username, description_photo } = photo;
+  return (
+    <article>
+      <header className="header">
+        <div className="perfilImage">
+          <Avatar avatar={user?.avatar} username={user?.username} />
+        </div>
+        <div className="photoDatas">
+          <p>{username}</p>
+        </div>
+      </header>
+      <div className="bodyPhoto">
+        <img
+          onClick={() => setModal(true)}
+          src={`${process.env.REACT_APP_API_URL}/${name_photo}`}
+          alt="Photo"
+        />
+      </div>
+      {modal ? (
+        <div>
+          <DetailsModal id={id} photo={name_photo} />
+        </div>
+      ) : null}
+      <div className="post-content">
+        <div className="iconsPhoto">
+          <LikeButton></LikeButton>
+          <CommentButton
+            commentInput={commentInput}
+            setCommentInput={setCommentInput}
+            id={id}
+          ></CommentButton>
+        </div>
+        <p className="likes">1000 likes</p>
+        <p className="description">
+          <span>{username}</span>
+          {description_photo}
+        </p>
+      </div>
+    </article>
+  );
 };
 
 export default Photo;
